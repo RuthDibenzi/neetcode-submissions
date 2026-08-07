fa62a655e698ae5c9eab4fca1dd770cde038fdf5
Ruth Dibenzi <144690425+RuthDibenzi@users.noreply.github.com>
2026-08-07 11:38:18 +0100

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,13 @@
+a, b, c = 2, 8, 5
+
+total_product = a * b // c 
+print(total_product)
+
+remainder_product = a * b % c
+print(remainder_product)
+
+result1 = a ** b
+print(result1)
+
+result2 = b ** c 
+print(result2)
